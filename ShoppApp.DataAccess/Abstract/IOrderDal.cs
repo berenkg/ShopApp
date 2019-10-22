c979cb5f001892f217d1b05de84a1ba9054d4d6f
Beren Kuday GÖRÜN <beren.kuday.g@gmail.com>
2019-10-22 13:54:09 +0300

    Interfaceler oluşturuldu ve IRepository ile implemen edildi

---
+++ ShoppApp.DataAccess/Abstract/IOrderDal.cs
@@ -0,0 +1,13 @@
+﻿using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppApp.DataAccess.Abstract
+{
+    public interface IOrderDal:IRepository<Order>
+    {
+        
+
+    }
+}
---
+++ ShoppApp.DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,14 @@
+﻿using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ShoppApp.DataAccess.Abstract
+{
+    public interface IProductDal:IRepository<Product>
+    {
+        IEnumerable<Product> GetPopularProducts();
+    }
+}

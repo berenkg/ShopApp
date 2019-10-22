d79ae38154414d6fb461c6e3c169b1ee83050cad
Beren Kuday GÖRÜN <beren.kuday.g@gmail.com>
2019-10-22 17:31:57 +0300

    Product ve Category tablolarını birbirne bağlamak için düzeltildi.

--- ShopApp.Entities/ProductCategory.cs
+++ ShopApp.Entities/ProductCategory.cs
@@ -6,7 +6,6 @@ namespace ShopApp.Entities
 {
     public class ProductCategory
     {
-        public int Id { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
         public int ProductId { get; set; }

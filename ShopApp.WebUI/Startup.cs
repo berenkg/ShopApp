da91b536c08fc0570ca688997b7aed7504f0c923
Beren Kuday GÖRÜN <beren.kuday.g@gmail.com>
2019-10-23 11:59:38 +0300

    Static Files için ayarlama yapıldı

--- ShopApp.WebUI/Startup.cs
+++ ShopApp.WebUI/Startup.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using ShopApp.Business.Abstract;
 using ShopApp.Business.Concrete;
 using ShoppApp.DataAccess.Abstract;
@@ -40,6 +42,14 @@ namespace ShopApp.WebUI
                 SeedDatabase.Seed();
             }
             app.UseMvcWithDefaultRoute();
-        }
+            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+            Path.Combine(Directory.GetCurrentDirectory(), "wwwdata")),
+                RequestPath = "/lib"
+            });
+        
+    }
     }
 }
